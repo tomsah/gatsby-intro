c890bc4778b088d7938a9a997c7a0f5db29c234c
Thomas Salah <thomas.salah@gmail.com>
2020-07-21 15:06:54 +0100

    Feat(basic): simple nav and routing to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import {Link} from 'gatsby'
-
 export default () => (
   <>
-    <h1>Home !!</h1>
-    <p>Hello london</p>
-    <Link to="/about">&larr; About</Link>
+    <h1>About me</h1>
+    <p>My site.</p>
+    <Link to="/">&larr; Home</Link>
   </>
 )
