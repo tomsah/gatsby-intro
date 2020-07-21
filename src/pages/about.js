7b81eee286efadb87de9129d837abee966754e25
Thomas Salah <thomas.salah@gmail.com>
2020-07-21 16:19:59 +0100

    feat: add layout with global styling and header

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import Layout from '../components/layout'
 export default () => (
-  <>
+  <Layout>
     <h1>About me</h1>
     <p>My site.</p>
     <Link to="/">&larr; Home</Link>
-  </>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link} from 'gatsby'
 
+import Layout from '../components/layout'
+
 export default () => (
-  <>
+  <Layout>
     <h1>Home !!</h1>
     <p>Hello london</p>
     <Link to="/about">&larr; About</Link>
-  </>
+  </Layout>
 )
