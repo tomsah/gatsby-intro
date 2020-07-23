ebb5157418ef2806abbcd20816d51abbb98f00e7
Thomas Salah <thomas.salah@gmail.com>
2020-07-23 14:57:32 +0100

    feat: hero image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,20 @@ import usePosts from '../hooks/use-posts'
 
 import Layout from '../components/layout'
 import PostPreview from '../components/post-preview'
+import Hero from '../components/hero'
 
 export default () => {
   const posts = usePosts()
 
   return (
-    <Layout>
-      <h1>Home !!</h1>
-      <p>Hello london</p>
-      <Link to="/about">About &rarr;</Link>
-
-      <h2>Read my Blog</h2>
-      {posts.map((post) => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my Blog</h2>
+        {posts.map((post) => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   )
 }
